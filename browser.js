b3d26eea0a08fb7d2e435b4c780dfbe37f29afcf
Denys Dovhan <denysdovhan@gmail.com>
2017-11-18 02:48:45 +0200

    Put active user name on topbar

--- browser.js
+++ browser.js
@@ -1,1 +1,22 @@
-console.log('hello, world!')
+const $ = document.querySelector.bind(document);
+
+document.addEventListener('DOMContentLoaded', () => {
+  // do your setup here
+
+  // Change application title on login page
+  const appTitle = $('.wrapper .banner h1');
+  if (appTitle) {
+    appTitle.innerHTML = 'Inboxer'
+  }
+
+  // Put the name of active user on the topbar
+  const activeUserName = $('div.gb_Bb.gb_Cb');
+  const topbarUserLink = $('a.gb_b.gb_gb.gb_R');
+  if (activeUserName && topbarUserLink) {
+    const span = document.createElement('span');
+    span.textContent = activeUserName.textContent;
+    span.classList.add('active-user-name');
+    topbarUserLink.appendChild(span);
+  }
+});
+
