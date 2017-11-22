a436a227a0795940a45a2ee1f389fa93cbc14079
Denys Dovhan <denysdovhan@gmail.com>
2017-11-22 12:30:49 +0200

    Improve .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,10 @@
 module.exports = {
-    "extends": "airbnb-base"
+    "extends": "airbnb-base",
+    "env": {
+        "browser": true,
+        "node": true,
+    },
+    "rules": {
+        "import/no-extraneous-dependencies": [2, { "devDependencies": true }]
+    }
 };
